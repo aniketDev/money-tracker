0673ffc3d021c02a4397c1fb8da7a81b92ff04ae
Aniket Mandal <aniketmandal9@gmail.com>
2022-11-22 23:45:13 +0530

    Redux update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,59 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import configureStore from './store';
-import RootNavigator from './navigation';
-import { useFonts } from 'expo-font';
-import { useAssets } from 'expo-asset';
-import AppLoading from 'expo-app-loading';
+import React, {useEffect, useCallback, useState} from "react";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+// import RootNavigator from './navigation';
+import { Text, View } from "react-native";
+import { loadAsync } from "expo-font";
+import { preventAutoHideAsync, hideAsync } from "expo-splash-screen";
+import Entypo from "@expo/vector-icons/Entypo";
 
 const initialData = {};
 const store = configureStore(initialData);
-
-let customFonts = {
-  'open-sans': require('../assets/fonts/OpenSans/OpenSans-Regular.ttf'),
-  'open-sans-bold': require('../assets/fonts/OpenSans/OpenSans-Bold.ttf'),
-  'open-sans-light': require('../assets/fonts/OpenSans/OpenSans-Light.ttf'),
-  'open-sans-semibold': require('../assets/fonts/OpenSans/OpenSans-SemiBold.ttf')
-};
-let assets = [];
+preventAutoHideAsync();
 
 export default () => {
-  let [fontsLoaded, fontLoadingError] = useFonts(customFonts);
-  let [assetsLoaded, assetsLoadingError] = useAssets(assets);
-  if (!fontsLoaded || !assetsLoaded) {
-    return <AppLoading />;
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await loadAsync(Entypo.font);
+        // await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
   return (
     <Provider store={store}>
-      <RootNavigator />
+      <View
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        onLayout={onLayoutRootView}
+      >
+        <Text>SplashScreen Demo! 👋</Text>
+        <Entypo name="rocket" size={30} />
+      </View>
     </Provider>
   );
 };
